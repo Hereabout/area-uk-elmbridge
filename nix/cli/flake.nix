f358a505a91f878b6d708eca33809a5684278f0c
Phil Curzon <phil@novelfs.org>
2024-10-24 16:37:21 +0100

    Add ref=main

--- nix/cli/flake.nix
+++ nix/cli/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
      flake-utils.url = "github:numtide/flake-utils";
-     hereaboutPkg.url = "git+ssh://git@github.com/Hereabout/hereabout-mono";
+     hereaboutPkg.url = "git+ssh://git@github.com/Hereabout/hereabout-mono?ref=main";
   };
 
   outputs = { self, nixpkgs, flake-utils, hereaboutPkg }:
